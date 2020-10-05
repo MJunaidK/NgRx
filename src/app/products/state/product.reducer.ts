c4130cc364aa60220938b5da5c148dbfea27783e
MJunaidK <junaidcts@gmail.com>
2020-10-04 20:33:12 -0400

    Defining interface for slice of state

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,4 +1,11 @@
 import { createAction, createReducer, on } from '@ngrx/store';
+import { Product } from '../product';
+
+export interface ProductState{
+    showProductCode: boolean;
+    currentProduct: Product;
+    prodcuts: Product[]
+}
 
 export const prodcutReducer =  createReducer(
     { showProductCode: true },
